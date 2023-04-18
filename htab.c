766c1bbb343a6c053296a67d2b18cbbade0f5aff
Frank3r <frarantas@gmail.com>
2023-04-18 20:32:27 +0200

    added htab_size, htab_bucket_count, htab_find

--- htab.c
+++ htab.c
@@ -11,7 +11,7 @@
 struct htab{
     size_t size;
     size_t arr_size;
-    struct htab_item_t **arr_ptr;
+    htab_item_t **arr_ptr;
 };
 
 /*  Hash function
@@ -42,5 +42,30 @@ htab_t *htab_init(const size_t n){
         exit(0);
     }
     return tab;
+}
+
+size_t htab_size(const htab_t * t){
+    return t->size;
+}
+
+size_t htab_bucket_count(const htab_t * t){
+    return t->arr_size;
+}
+
+htab_pair_t * htab_find(const htab_t * t, htab_key_t key){
+    size_t key_hash= htab_hash_function(key);
+    htab_item_t *cur_ptr=NULL;
+    for(size_t i = 0; i < t->arr_size;i++){
+        if(t->arr_ptr[i]==NULL){ // firsthand empty check
+            continue;
+        }
+        cur_ptr=t->arr_ptr[i];
+        while(cur_ptr!=NULL){
+            if(key_hash== htab_hash_function(cur_ptr->pair.key)){
+                return &cur_ptr->pair;
+            }
+        }
+    }
+    return NULL;
 }
 
